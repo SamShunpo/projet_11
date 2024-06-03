9bcda7a8f97da364055d9f82371d4e45ea96d066
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-06-03 11:30:23 +0200

    Edit Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,31 @@
 import Header from '../../components/Header/Header'
+import HomeCard from '../../components/HomeCard/HomeCard'
+import imgChart from '../../assets/icon-chat.webp'
+import imgMoney from '../../assets/icon-money.webp'
+import imgSecurity from '../../assets/icon-security.webp'
+import Footer from '../../components/Footer/Footer'
 
 function Home() {
     return (
-        <div>
+        <div className='react-div'>
             <Header />
             <main>
-                <div class="hero">
-                    <section class="hero-content">
-                        <h2 class="sr-only">Promoted Content</h2>
-                        <p class="subtitle">No fees.</p>
-                        <p class="subtitle">No minimum deposit.</p>
-                        <p class="subtitle">High interest rates.</p>
-                        <p class="text">Open a savings account with Argent Bank today!</p>
+                <div className="hero">
+                    <section className="hero-content">
+                        <h2 className="sr-only">Promoted Content</h2>
+                        <p className="subtitle">No fees.</p>
+                        <p className="subtitle">No minimum deposit.</p>
+                        <p className="subtitle">High interest rates.</p>
+                        <p className="text">Open a savings account with Argent Bank today!</p>
                     </section>
                 </div>
+                <section class="features">
+                    <HomeCard img={imgChart} title={"You are our #1 priority"} description={"Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."}/>
+                    <HomeCard img={imgMoney} title={"More savings means higher rates"} description={"The more you save with us, the higher your interest rate will be!"}/>
+                    <HomeCard img={imgSecurity} title={"Security you can trust"} description={"We use top of the line encryption to make sure your data and moneyis always safe."}/>
+                </section>
             </main>
-
+            <Footer />
         </div>
     )
 }
