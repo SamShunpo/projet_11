00d9750d25be845a9222bae08c53fd0c650136cc
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-06-03 11:29:33 +0200

    Edit Header page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,18 @@ import logo from '../../assets/argentBankLogo.webp'
 import { Link } from 'react-router-dom'
 
 function Header() {
-    return(
+    return (
         <nav className='main-nav'>
-            <Link to="/" className='main-nav-logo'><img src={logo} alt='logo ArgentBank' className='main-nav-logo-image'/></Link>
-            <div>
-            <Link to="/SignIn" className='main-nav-item'><i class="fa fa-user-circle"></i> Sign In </Link>
+            <Link to="/" className='main-nav-logo'><img src={logo} alt='logo ArgentBank' className='main-nav-logo-image' /></Link>
+            <div className='login'>
+                <div>
+                    <Link to="/SignIn" className='main-nav-item'><i class="fa fa-user-circle"></i> Sign In </Link>
+                </div>
+                <div className='signout-invisible'>
+                    <Link to="/" className='main-nav-item'><i class="fa fa-sign-out"></i> Sign Out </Link>
+                </div>
             </div>
+
         </nav>
     )
 }
