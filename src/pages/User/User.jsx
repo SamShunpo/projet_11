580b063c24c57823cf954fa2632a573f73083852
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-06-03 11:29:18 +0200

    Edit user page

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,7 +1,22 @@
+import Header from "../../components/Header/Header"
+import Footer from "../../components/Footer/Footer"
+import Button from "../../components/Button/Button"
+import BankAccount from "../../components/BankAccount/BankAccount"
+
 function User() {
-    return(
-        <div>
-            <p>User</p>
+    return (
+        <div className="react-div">
+            <Header />
+            <main className="main bg-dark">
+                <div className="header">
+                    <h1>Welcome back <br/> Tony Jarvis!</h1>
+                    <Button classname="edit-button" title="Edit Name"/>
+                </div>
+                <BankAccount title="Argent Bank Checking (x8349)" amount="$2,082.79" description="Available Balance"/>
+                <BankAccount title="Argent Bank Savings (x6712)" amount="$10,928.42" description="Available Balance"/>
+                <BankAccount title="Argent Bank Credit Card (x8349)" amount="$184.30" description="Current Balance"/>
+            </main>
+            <Footer />
         </div>
     )
 }
