561b5da802d4107d4f5f5e94d1d4c8c9066c04e4
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-06-03 11:27:57 +0200

    Delete Edit and transaction page

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import SignIn from './pages/SignIn/SignIn'
-import Transaction from './pages/Transaction/Transaction'
-import EditUser from './pages/EditUser/EditUser'
 import User from './pages/User/User'
 import './index.css'
 
@@ -14,8 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/User" element={<User />}></Route>
-        <Route path="/EditUser" element={<EditUser />}></Route>
-        <Route path="/Transaction" element={<Transaction />}></Route>
         <Route path="/SignIn" element={<SignIn />}></Route>
       </Routes>
     </BrowserRouter>
