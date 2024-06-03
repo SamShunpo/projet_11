f95324c340a083c2b8a427331566619aa723cd8e
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-06-03 11:30:37 +0200

    Edit SignIn page

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,7 +1,31 @@
+import Footer from "../../components/Footer/Footer"
+import Header from "../../components/Header/Header"
+import { Link } from 'react-router-dom'
+
 function SignIn() {
-    return(
-        <div>
-            <p>SignIn</p>
+    return (
+            <div className="react-div">
+            <Header />
+            <main className="main bg-dark">
+                <section className="sign-in-content">
+                    <i className="fa fa-user-circle sign-in-icon"></i>
+                    <h1>Sign In</h1>
+                    <form>
+                        <div className="input-wrapper">
+                            <label for="username">Username</label><input type="text" id="username" />
+                        </div>
+                        <div className="input-wrapper">
+                            <label for="password">Password</label><input type="password" id="password" />
+                        </div>
+                        <div className="input-remember">
+                            <input type="checkbox" id="remember-me" /><label for="remember-me"
+                            >Remember me</label>
+                        </div>
+                        <Link to="/User" className="sign-in-button">Sign In</Link>
+                    </form>
+                </section>
+            </main>
+            <Footer />
         </div>
     )
 }
